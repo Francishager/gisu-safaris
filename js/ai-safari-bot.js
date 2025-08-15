cd81271de94f4c2efff2bcaac6d74cd3bb60eb01
Francishager <87711592+Francishager@users.noreply.github.com>
2025-08-15 14:14:46 -0700

    commit  41

--- js/ai-safari-bot.js
+++ js/ai-safari-bot.js
@@ -336,7 +336,7 @@ class SafariAIBot {
                 padding: 1px 10px;
                 font-size: 14px;
                 outline: none;
-                margin-right: 27px;
+                margin-right: 17px;
                
             }
 
